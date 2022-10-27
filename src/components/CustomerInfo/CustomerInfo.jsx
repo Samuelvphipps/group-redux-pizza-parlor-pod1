4c512e67783f8a5b4587921cb70b8d5c73169d2a
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-27 11:28:01 -0500

    capture form value

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -4,6 +4,15 @@ function CustomerInfo () {
 
     const [checkedVal, setCheckedVal] = useState('')
 
+    const [formVal, setFormVal] = useState({
+        customer_name: '',
+        street_address: '',
+        city: '',
+        zip: '',
+        type: ''
+    })
+
+    
     const onChecked = (value) => {
         console.log('in onChecked')
         setCheckedVal(value);
@@ -11,19 +20,26 @@ function CustomerInfo () {
 
     }
 
-    const onSubmit = () => {
-        
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        setFormVal({
+            customer_name: evt.target.customer_name.value,
+            street_address: evt.target.street_address.value,
+            city: evt.target.city.value,
+            zip: evt.target.zip.value,
+            type: checkedVal
+        });
     }
 
     
     return (
         <>
         <h1>Step 2:</h1>
-        <form onSubmit={onSubmit}>
-            <input type="text" placeholder="Name"></input>
-            <input type="text" placeholder="Street Address"></input>
-            <input type="text" placeholder="City"></input>
-            <input type="text" placeholder="Zip"></input>
+        <form onSubmit={(evt)=>onSubmit(evt)}>
+            <input type="text" name="customer_name" placeholder="Name"></input>
+            <input type="text" name="street_address" placeholder="Street Address"></input>
+            <input type="text" name="city" placeholder="City"></input>
+            <input type="text" name="zip" placeholder="Zip"></input>
             <div>
                 <label>Pickup</label>
                 <input value="Pickup" onChange={()=>onChecked('pickup')} required name="type" type="radio"></input>
